9a62233b94a94d19b32391ad0a4c784d8a2676b3
Matt Lim <mlimcollege@gmail.com>
2021-10-31 10:46:47 -0700

    Copy fonts and colors from Creator Inbox

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,31 @@
 import "@/css/global/Global.css";
 
+// Colors
+import "@/css/global/colors/ColorVariables.css";
+import "@/css/global/colors/BackgroundColorClasses.css";
+import "@/css/global/colors/ColorClasses.css";
+
+// Fonts
+import "@/css/global/fonts/FontVariables.css";
+import "@/css/global/fonts/FontClasses.css";
+
+import Head from "next/head";
+
 import { AppProps } from "next/app";
 
+const META_DESCRIPTION =
+  "A tool for easily find PDAs (program derived addresses)";
+
 function App({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Solana PDA Finder</title>
+        <meta name="description" content={META_DESCRIPTION} />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default App;
